0165dc479a2d591d06df32bd87d30acfa27f4767
Morgan <160355750+SirMeOWski22@users.noreply.github.com>
2024-05-09 23:45:13 -0500

    updated json, tested logo maker, updated code based on failed tests

--- src/svgGenerator.js
+++ src/svgGenerator.js
@@ -1,9 +1,11 @@
+const fs = require('fs');
+
 function generateSVG(userInput) {
   const { text, textColor, shape, shapeColor } = userInput;
   const svgContent = `<svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-      <rect width="100%" height="100%" fill="${shapeColor}"/>
-      <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="${textColor}">${text}</text>
-    </svg>`;
+  <rect width="100%" height="100%" fill="${shapeColor}"/>
+  <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="120px" font-family="Blippo, fantasy">${text}</text>
+</svg>`;
 
   fs.writeFileSync('logo.svg', svgContent);
 }
--- test/svgGenerator.test.js
+++ test/svgGenerator.test.js
@@ -2,7 +2,7 @@ const { generateSVG } = require('../src/svgGenerator');
 const fs = require('fs');
 
 describe('SVG Generator', () => {
-  isTypedArray('generates SVG correctly', () => {
+  it('generates SVG correctly', () => {
     const userInput = {
       text: 'ABC',
       textColor: '#ff0000',
@@ -13,9 +13,9 @@ describe('SVG Generator', () => {
     generateSVG(userInput);
 
     const svgContent = fs.readFileSync('logo.svg', 'utf-8');
-    expect(svgContent).toContain('svg');
+    expect(svgContent).toContain('<svg');
     expect(svgContent).toContain('<text');
-    expect(svgContent).toContain('<rect>');
+    expect(svgContent).toContain('<rect');
     expect(svgContent).toContain('ABC');
     expect(svgContent).toContain('#ff0000');
     expect(svgContent).toContain('#0000ff');
